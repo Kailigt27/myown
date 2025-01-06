e2b7d58ffc55724b80ab0503404dcbdb39658f08
Kailigt27 <skytechtips@outlook.com>
2025-01-06 16:58:58 +0800

    nothing happened

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int number;
+        number = int.Parse(Console.ReadLine());
+        int n = Fbnq(number);
+        Console.WriteLine(n);
+    }
+    static int Fbnq(int s)
+    {
+        if (s==0)
+        {
+            return 0;
+        }
+        if (s==1)
+        {
+            return 1;
+        }
+        return Fbnq(s - 1) + Fbnq(s- 2);
+    }
+}
